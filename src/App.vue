7d318d64272c1c11e14b6b1323f72f629a488a16
Tiru <tiru@live.in>
2021-06-09 17:51:19 +0530

    added table

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>Hello</div>
+    <div>Hello
+    
+     <div>    
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Device</th>
+              <th scope="col">RequestedDate</th>
+              <th scope="col">id</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="result in results">
+              <td>{{ result.Device }}</td>
+              <td>{{ result.RequestedDate }}</td>
+              <td>{{ result.id }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>   
+
+    </div>
 </template>
 
 
