ed5673c65c9f5d0f5e9d094946a9373b4910da06
Tiru <tiru@live.in>
2021-06-09 17:45:28 +0530

    test

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,8 @@
 <template>
     <div>Hello</div>
-    <div>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">Device</th>
-                    <th scope="col">DeviceType</th>
-                    <th scope="col">id</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="result in results">
-                    <td>{{ result.deviceName }}</td>
-                    <td>{{ result.deviceType }}</td>
-                    <td>{{ result.deviceId }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
 </template>
 
-<!--
+
 <script>
-import axios from 'axios';
 
-    export default {
-        data() {
-            return {
-                results: [],
-                errors: []
-            }
-        },
-
-        // Fetches posts when the component is created.
-        created() {
-            axios.get(`/api/GetDevices`)
-                .then(response => {
-                    // JSON responses are automatically parsed.
-                    this.results = response.data
-                })
-                .catch(e => {
-                    this.errors.push(e)
-                })
-        }
-    };
-</script> -->
+</script> 
