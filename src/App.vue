8b009c8a50b738be35c5fbab9f1799095807d536
Tiru <tiru@live.in>
2021-06-10 10:05:38 +0530

    vue updated

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>Hello All
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Device</th>
+              <th scope="col">RequestedDate</th>
+              <th scope="col">id</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="result in results" :key="result.id">
+              <td>{{ result.Device }}</td>
+              <td>{{ result.RequestedDate }}</td>
+              <td>{{ result.id }}</td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+</template>
+
+
+<script>
+const url = "../api/getdevices";
+
+const vm = new Vue({
+        el: '#app',
+        data: {
+          results: []
+        },
+        mounted() {
+          axios.get(url).then(response => {
+            this.results = response.data
+          })
+        }
+      });
+
+</script> 
